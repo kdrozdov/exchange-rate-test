567ddc26b00e01239423727cf9ff045a9a118614
Kirill Drozdov <kirill.dzv@gmail.com>
2017-12-01 18:38:30 +0500

    fix factory

--- spec/factories/exchange_rate.rb
+++ spec/factories/exchange_rate.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :exchange_rate do
     value 58.31
     nominal 1
-    datetime '01-12-2017 12:44'
+    datetime { DateTime.current + 1.hour }
 
 
     trait :invalid do
